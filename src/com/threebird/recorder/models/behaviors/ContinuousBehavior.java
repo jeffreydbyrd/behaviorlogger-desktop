c64c07deb4c90ca49801579ba41394d5eccb022f
biffbyrd <jeffreydbyrd@gmail.com>
2014-10-22 22:45:37 -0400

    renames DurationalBehavior to ContinuousBehavior because it sounds better (and it's more sciency)

--- src/com/threebird/recorder/models/behaviors/ContinuousBehavior.java
+++ src/com/threebird/recorder/models/behaviors/ContinuousBehavior.java
@@ -1,12 +1,12 @@
 package com.threebird.recorder.models.behaviors;
 
-public class DurationalBehavior implements Behavior
+public class ContinuousBehavior implements Behavior
 {
   final public String description;
   final public Integer start;
   final public Integer end;
 
-  public DurationalBehavior( String description, Integer start, Integer end )
+  public ContinuousBehavior( String description, Integer start, Integer end )
   {
     this.description = description;
     this.start = start;
