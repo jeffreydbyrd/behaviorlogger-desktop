b033372d6a7ad31000b79c2dce0f2c7350225301
biffbyrd <jeffreydbyrd@gmail.com>
2015-08-20 06:57:54 -0400

    adds continuous keys to the time-window summary

--- src/main/java/com/threebird/recorder/views/ioa/IoaTimeWindowSummary.java
+++ src/main/java/com/threebird/recorder/views/ioa/IoaTimeWindowSummary.java
@@ -8,7 +8,6 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
-import javafx.scene.text.Text;
 
 import com.threebird.recorder.utils.ioa.TimeWindowCalculations;
 
@@ -17,9 +16,9 @@ public class IoaTimeWindowSummary extends VBox
   public IoaTimeWindowSummary( Map< Character, TimeWindowCalculations > ioaDiscrete,
                                Map< Character, Double > ioaContinuous )
   {
-    Text txt = new Text( "IOA Discrete Time Window Summary" );
-    txt.setFont( Font.font( "Verdana", 12 ) );
-    this.getChildren().add( txt );
+    Label discreteLbl = new Label( "IOA Discrete Time Window Summary" );
+    discreteLbl.setFont( Font.font( "Verdana", 12 ) );
+    this.getChildren().add( discreteLbl );
 
     for (Entry< Character, TimeWindowCalculations > entry : ioaDiscrete.entrySet()) {
       Character key = entry.getKey();
@@ -30,9 +29,29 @@ public class IoaTimeWindowSummary extends VBox
       left.setAlignment( Pos.TOP_RIGHT );
 
       Label mid = new Label( " - " + calc.result1 );
-      Label right = new Label( ", " + calc.result1 );
+      Label right = new Label( ", " + calc.result2 );
 
       HBox row = new HBox( left, mid, right );
+      row.setSpacing( 5 );
+
+      this.getChildren().add( row );
+    }
+
+    Label continuousLbl = new Label( "IOA Continuous Time Window Summary" );
+    continuousLbl.setFont( Font.font( "Verdana", 12 ) );
+    this.getChildren().add( continuousLbl );
+
+    for (Entry< Character, Double > entry : ioaContinuous.entrySet()) {
+      Character key = entry.getKey();
+      Double avg = entry.getValue();
+
+      Label left = new Label( "" + key );
+      left.setMinWidth( 25 );
+      left.setAlignment( Pos.TOP_RIGHT );
+
+      Label right = new Label( " - " + avg );
+
+      HBox row = new HBox( left, right );
       row.setSpacing( 5 );
 
       this.getChildren().add( row );
