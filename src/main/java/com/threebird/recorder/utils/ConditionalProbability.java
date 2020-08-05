8d69faa7a66ed308ac767bc75e64a74539816d89
jeffreydbyrd <jeffreydbyrd@gmail.com>
2020-08-05 08:35:52 -0400

    remove some duplication

--- src/main/java/com/threebird/recorder/utils/ConditionalProbability.java
+++ src/main/java/com/threebird/recorder/utils/ConditionalProbability.java
@@ -2,6 +2,7 @@ package com.threebird.recorder.utils;
 
 import java.util.List;
 import java.util.Map;
+import java.util.function.BiFunction;
 import java.util.stream.Collectors;
 
 import com.google.common.collect.Lists;
@@ -24,6 +25,30 @@ public class ConditionalProbability {
 
     public static Map<Integer, Double> binary(KeyBehaviorMapping target, KeyBehaviorMapping consequence,
 	    List<BehaviorEvent> events, boolean establishingOperations) {
+	return calculate( //
+		target, //
+		consequence, //
+		events, //
+		establishingOperations, //
+		ConditionalProbability::calcBinary);
+    }
+
+    public static Map<Integer, Double> proportion(KeyBehaviorMapping target, KeyBehaviorMapping consequence,
+	    List<BehaviorEvent> events, boolean establishingOperations) {
+	return calculate( //
+		target, //
+		consequence, //
+		events, //
+		establishingOperations, //
+		ConditionalProbability::calcProportion);
+    }
+
+    private static Map<Integer, Double> calculate( //
+	    KeyBehaviorMapping target, //
+	    KeyBehaviorMapping consequence, //
+	    List<BehaviorEvent> events, //
+	    boolean establishingOperations, //
+	    BiFunction<List<BehaviorEvent>, List<BehaviorEvent>, Map<Integer, Double>> calc) {
 	List<BehaviorEvent> consequentEvents = events.stream() //
 		.filter((e) -> e.key == consequence.key) //
 		.collect(Collectors.toList());
@@ -37,6 +62,11 @@ public class ConditionalProbability {
 		    .collect(Collectors.toList());
 	}
 
+	return calc.apply(consequentEvents, targetEvents);
+    }
+
+    private static Map<Integer, Double> calcBinary(List<BehaviorEvent> consequentEvents,
+	    List<BehaviorEvent> targetEvents) {
 	Map<Integer, Double> results = Maps.newHashMap();
 	double numOccurrencesOfConsequenceFollowingTarget;
 	for (Integer range : RANGES) {
@@ -51,21 +81,8 @@ public class ConditionalProbability {
 	return results;
     }
 
-    public static Map<Integer, Double> proportion(KeyBehaviorMapping target, KeyBehaviorMapping consequence,
-	    List<BehaviorEvent> events, boolean establishingOperations) {
-	List<BehaviorEvent> consequentEvents = events.stream() //
-		.filter((e) -> e.key == consequence.key) //
-		.collect(Collectors.toList());
-	List<BehaviorEvent> targetEvents = events.stream() //
-		.filter((e) -> e.key == target.key) //
-		.collect(Collectors.toList());
-
-	if (establishingOperations) {
-	    targetEvents = targetEvents.stream() //
-		    .filter((e) -> !hasOverlappingEvents(consequentEvents, e)) //
-		    .collect(Collectors.toList());
-	}
-
+    private static Map<Integer, Double> calcProportion(List<BehaviorEvent> consequentEvents,
+	    List<BehaviorEvent> targetEvents) {
 	Map<Integer, Double> results = Maps.newHashMap();
 	double totalDurationOfConsequenceFollowingTarget;
 	for (Integer range : RANGES) {
@@ -89,6 +106,15 @@ public class ConditionalProbability {
 	return results;
     }
 
+    private static boolean hasOverlappingEvents(List<BehaviorEvent> candidates, BehaviorEvent target) {
+	for (BehaviorEvent ce : candidates) {
+	    if (ce.startTime < target.startTime && ce.endTime() > target.startTime) {
+		return true;
+	    }
+	}
+	return false;
+    }
+
     private static boolean hasConsequentEvents(List<BehaviorEvent> candidates, BehaviorEvent target, int range) {
 	int end = target.startTime + range;
 	for (BehaviorEvent ce : candidates) {
@@ -105,14 +131,5 @@ public class ConditionalProbability {
 	return candidates.stream() //
 		.filter((ce) -> ce.startTime > target.startTime && ce.startTime < end) //
 		.collect(Collectors.toList());
-    }
-
-    private static boolean hasOverlappingEvents(List<BehaviorEvent> candidates, BehaviorEvent target) {
-	for (BehaviorEvent ce : candidates) {
-	    if (ce.startTime < target.startTime && ce.endTime() > target.startTime) {
-		return true;
-	    }
-	}
-	return false;
     }
 }
